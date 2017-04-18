41d5999efdc099bc5a197567d225b562e9755144
phillyai <sonagi.philly@gmail.com>
2017-04-18 10:16:40 +0900

    초기 커밋

---
+++ Asoch/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Asoch
+{
+    public class Client
+    {
+        private Socket _socket;
+        public Client()
+        {
+
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void DisConnect()
+        {
+
+        }
+    }
+}
---
+++ Asoch/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Asoch
+{
+    public class Server
+    {
+        private Socket _socket;
+        public Server()
+        {
+
+        }
+    }
+}
