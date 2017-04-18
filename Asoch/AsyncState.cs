a5071ed9937a684bb38723ef43f5e0e30d274826
phillyai <sonagi.philly@gmail.com>
2017-04-18 11:21:19 +0900

    클라이언트 Connect 구현

---
+++ Asoch/AsyncState.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+namespace Asoch
+{
+    public class AsyncState
+    {
+        public byte[] Buffer;
+        public Socket Socket;
+        public AsyncState(int size)
+        {
+            Buffer = new byte[size];
+        }
+    }
+}
--- Asoch/Client.cs
+++ Asoch/Client.cs
@@ -9,20 +9,63 @@ namespace Asoch
 {
     public class Client
     {
+        #region Private members
+        private ManualResetEvent _connectEvent;
         private Socket _socket;
-        public Client()
-        {
+        #endregion
 
+        #region Properties
+        public string Host { get; private set; }
+        public int Port { get; private set; }
+
+        public bool IsConnected { get; private set; } = false;
+#endregion
+
+        public Client(string hostIP, int port)
+        {
+            Host = hostIP; Port = port;
+            _connectEvent = new ManualResetEvent(false);
         }
 
-        public void Connect()
+        #region Connect
+        public bool Connect()
         {
+            _connectEvent.Reset();
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            _socket.BeginConnect(Host, Port, new AsyncCallback(ConnectCallback), _socket);
+            _connectEvent.WaitOne();
+            return IsConnected;
+        }
 
+        private void ConnectCallback(IAsyncResult res)
+        {
+            try
+            {
+                Socket client = res.AsyncState as Socket;
+                client.EndConnect(res);
+            }
+            catch(SocketException)
+            {
+                IsConnected = false;
+            }
+            finally
+            {
+                _connectEvent.Set();
+            }
         }
 
         public void DisConnect()
         {
-
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
         }
+        #endregion
+
+        #region Send
+
+        #endregion
+
+        #region Receive
+        #endregion
     }
 }
